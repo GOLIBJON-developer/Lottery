094fe41c816b42384dfab62d14197249457ecfb7
GOLIBJON <81797313+GOLIBJON-developer@users.noreply.github.com>
2026-04-15 23:05:09 +0500

    Use environment variables for project ID and RPC URL

--- raffle-ui/lib/wagmi.ts
+++ raffle-ui/lib/wagmi.ts
@@ -1,10 +1,14 @@
 'use client'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { sepolia } from 'wagmi/chains'
+import { http } from 'wagmi'
 
 export const config = getDefaultConfig({
   appName: 'Raffle',
-  projectId: 'YOUR_ID', // WalletConnect project ID (walletconnect.com)
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID, // WalletConnect project ID (walletconnect.com)
   chains: [sepolia],
   ssr: true,
+  transports: {
+    [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL),
+  },
 })
